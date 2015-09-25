a2512109c9f50f5b4b2230261ddd635de0e5afc3
Hugh Ratsch <hugh.ratsch.18@gmail.com>
2015-09-25 01:06:37 -0500

    omniauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925033744) do
+ActiveRecord::Schema.define(version: 20150925053745) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20150925033744) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
